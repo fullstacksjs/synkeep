5b1e2d9136a05b09967ad7d43feb15ed22637816
Mohammad MohammadAlian <mralian.dev@gmail.com>
2021-03-24 18:15:27 +0430

    fix problem of getting ::ffff:127.0.0.1 instead of 127.0.0.1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const { httpServer } = require('./app');
 const io = require('./io');
 
-httpServer.listen(process.env.PORT || 3000, () => {
+httpServer.listen(process.env.PORT || 3000, '0.0.0.0', () => {
   console.log('Server Listening...');
 });
